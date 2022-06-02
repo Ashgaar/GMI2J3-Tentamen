16ea8ce6959be982f09a9b1b06f9aaaf13451c05
Mikael Olsson <zangiloxd@hotmail.com>
2022-06-02 11:47:12 +0200

    Mocking funtionality

--- IsbnLib.Tests/UnitTestMock.cs
+++ IsbnLib.Tests/UnitTestMock.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 
 namespace IsbnLib.Tests
 {
     public class UnitTestMock
     {
-        private ILibraryDbService<string> _libraryDbService;
-        public UnitTestMock()
+        private Mock<ILibraryDbService<string>> _libraryDbServiceMock;
+        
+        public UnitTestMock(/*ILibraryDbService<string> libraryDbService*/)
         {
-            //_libraryDbService = new LibraryDbServiceMock();
+            _libraryDbServiceMock = new Mock<ILibraryDbService<string>>();
         }
         
         [Fact]
-        public void Test()
+        public void TestLibraryLendBook()
         {
-
+            _libraryDbServiceMock.Setup(x => x.LendBook("123456789")).Returns(true);
+            var result = _libraryDbServiceMock.Object.LendBook("123456789");
+            Assert.True(result);
         }
 
+        [Fact]
+        public void TestLibraryReturnBook()
+        {
+            _libraryDbServiceMock.Setup(x => x.ReturnBook("123456789")).Returns(true);
+            var result = _libraryDbServiceMock.Object.ReturnBook("123456789");
+            Assert.True(result);
+        }
 
+        [Fact]
+        public void TestLibrarySearchBooks()
+        {
+            _libraryDbServiceMock.Setup(x => x.Search("123456789")).Returns(new List<string> { "123456789" });
+            var result = _libraryDbServiceMock.Object.Search("123456789");
+            Assert.True(result.Count == 1);
+        }        
     }
 }
--- IsbnLib/Interfaces/ILibraryDbService.cs
+++ IsbnLib/Interfaces/ILibraryDbService.cs
@@ -9,4 +9,4 @@ namespace IsbnLib.Interfaces
 
         List<T> Search(T searchFilter);
     }
-}
+}   
